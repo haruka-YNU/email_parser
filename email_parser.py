330ad76d2c322143f775214f853d800c53f30fd1
haruka <xiao-yao-tn@ynu.jp>
2017-07-15 19:46:33 +0900

    fix bug

--- email_parser.py
+++ email_parser.py
@@ -1,4 +1,4 @@
-# -*-coding:utf-8 -*-
+﻿# -*-coding:utf-8 -*-
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import email
@@ -28,7 +28,7 @@ class parser():
             return msg, file_hash
 
     def __save_file(self, file_name, data):
-        file_dir = os.path.join(self.save_dir, self.file_path.split('/')[-1])
+        file_dir = os.path.join(self.save_dir, self.file_path.split('/')[-1].split('.')[0]))
         file_path = os.path.join(file_dir, file_name)
         if not os.path.exists(file_dir):
             os.makedirs(file_dir)
