c900a3731c866008fbe89da5454527db1fd40553
haruka <xiao-yao-tn@ynu.jp>
2017-07-31 22:48:58 +0900

    small change

--- email_parser/bulk_parse.py
+++ email_parser/bulk_parse.py
@@ -5,27 +5,28 @@ import traceback
 import sys
 import os
 
-client = MongoClient('mongodb://192.168.10.107:27017/email')
+client = MongoClient('mongodb://127.0.0.1:27017/email')
 db = client.email
-tests = db.bulktests
+tests = db.newtests
 
-def batch_parse(email_path, save_path, error_path = '../error_log/'):
+def batch_parse(email_path, save_path, error_path = '../../error_log/'):
     count = 1
-    for i in os.listdir(email_path):
-        print count, i
-        try:
-            mail_par = parser(email_path + i, save_path)
-            result = mail_par.parse_email()
-            tests.insert_one(result)
-        except:
-            if not os.path.exists(error_path):
-                os.makedirs(error_path)
-            log = open(error_path + i + '.txt', 'w')
-            log.write(i)
-            traceback.print_exc(file=log)
-            log.flush()
-            log.close()
-        count +=1
+    for root, dirs, files in os.walk(email_path, topdown=False):
+        for name in files:
+            print count, name
+            try:
+                mail_par = parser(os.path.join(root, name), save_path)
+                result = mail_par.parse_email()
+                tests.insert_one(result)
+            except:
+                if not os.path.exists(error_path):
+                    os.makedirs(error_path)
+                log = open(error_path + name + '.txt', 'w')
+                log.write(name)
+                traceback.print_exc(file=log)
+                log.flush()
+                log.close()
+            count +=1
 
 if __name__ == '__main__':
     batch_parse(sys.argv[1], sys.argv[2])
