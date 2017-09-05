a984a8dd760b04f500d28aa723d15eedd0a0d453
haruka <xiao-yao-tn@ynu.jp>
2017-09-05 21:52:00 +0900

    small change

--- email_parser/bulk_parse.py
+++ email_parser/bulk_parse.py
@@ -5,28 +5,36 @@ import traceback
 import sys
 import os
 
-client = MongoClient('mongodb://127.0.0.1:27017/email')
+client = MongoClient('mongodb://xiao-mongodb:27017/email')
 db = client.email
-collection = db.newtests
+collection_name = sys.argv[1].split('/')[3]+'.'+sys.argv[1].split('/')[4]
+collection = db[collection_name]
 
-def batch_parse(email_path, save_path, error_path = '../../error_log/'):
+def batch_parse(base_path, start, end, error_path = '../../error_log/'):
+    continuous = range(int(start), int(end) + 1)
     count = 1
-    for root, dirs, files in os.walk(email_path, topdown=False):
-        for name in files:
-            print count, name
-            try:
-                mail_par = parser(os.path.join(root, name), save_path)
-                result = mail_par.parse_email()
-                collection.insert_one(result)
-            except:
-                if not os.path.exists(error_path):
-                    os.makedirs(error_path)
-                log = open(error_path + name + '.txt', 'w')
-                log.write(name)
-                traceback.print_exc(file=log)
-                log.flush()
-                log.close()
-            count +=1
+    for day in continuous:
+        day = str(day)
+        if len(day) < 2:
+            day = '0' + str(day)
+        email_path = os.path.join(base_path, day)
+        for root, dirs, files in os.walk(email_path, topdown=False):
+            for name in files:
+                print count, name
+                try:
+                    mail_par = parser(os.path.join(root, name), save_dir = '/mnt/AttachFiles/')
+                    result = mail_par.parse_email()
+                    collection.insert_one(result)
+                except:
+                    if not os.path.exists(error_path):
+                        os.makedirs(error_path)
+                    log = open(error_path + name + '.txt', 'w')
+                    log.write(name)
+                    traceback.print_exc(file=log)
+                    log.flush()
+                    log.close()
+                count +=1
 
 if __name__ == '__main__':
-    batch_parse(sys.argv[1], sys.argv[2])
+    batch_parse(sys.argv[1], sys.argv[2], sys.argv[3])
+	#sudo python /mnt/AttachFiles/2017/06 07 12
